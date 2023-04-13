0926790c8653bc269a198154c23d7c87b01fdd12
maurobortolani <mauro.bortolani@ialedu.it>
2023-04-13 14:59:34 +0200

    Update SensoreRFID.ino

--- SensoreRFID/SensoreRFID.ino
+++ SensoreRFID/SensoreRFID.ino
@@ -14,28 +14,28 @@ const char* mqtt_server = "broker.hivemq.com";
 WiFiClient espClient;
 PubSubClient client(espClient);
 unsigned long lastMsg = 0;
-#define MSG_BUFFER_SIZE	(50)
+#define MSG_BUFFER_SIZE (50)
 char msg[MSG_BUFFER_SIZE];
 int value = 0;
- 
-MFRC522 rfid(SS_PIN, RST_PIN); 
 
-MFRC522::MIFARE_Key key; 
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+MFRC522::MIFARE_Key key;
 
 byte nuidPICC[4];
+int deltatime = 0;
 
-void setup() { 
+void setup() {
   Serial.begin(9600);
-  SPI.begin(); 
-  rfid.PCD_Init(); 
+  SPI.begin();
+  rfid.PCD_Init();
 
-  pinMode(BUILTIN_LED, OUTPUT);     
+  pinMode(BUILTIN_LED, OUTPUT);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
 
-  for (byte i = 0; i < 6; i++) 
-  {
+  for (byte i = 0; i < 6; i++) {
     key.keyByte[i] = 0xFF;
   }
 
@@ -79,13 +79,12 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
   // Switch on the LED if an 1 was received as first character
   if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    digitalWrite(BUILTIN_LED, LOW);  // Turn the LED on (Note that LOW is the voltage level
     // but actually the LED is on; this is because
     // it is active low on the ESP-01)
   } else {
     digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
   }
-
 }
 
 void reconnect() {
@@ -111,15 +110,15 @@ void reconnect() {
     }
   }
 }
- 
+
 void loop() {
 
   // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-  if ( ! rfid.PICC_IsNewCardPresent())
+  if (!rfid.PICC_IsNewCardPresent())
     return;
 
   // Verify if the NUID has been readed
-  if ( ! rfid.PICC_ReadCardSerial())
+  if (!rfid.PICC_ReadCardSerial())
     return;
 
   Serial.print(F("PICC type: "));
@@ -127,42 +126,37 @@ void loop() {
   Serial.println(rfid.PICC_GetTypeName(piccType));
 
   // Check is the PICC of Classic MIFARE type
-  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
-    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
-    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI && piccType != MFRC522::PICC_TYPE_MIFARE_1K && piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
     Serial.println(F("Your tag is not of type MIFARE Classic."));
     return;
   }
 
-  if (rfid.uid.uidByte[0] != nuidPICC[0] || 
-    rfid.uid.uidByte[1] != nuidPICC[1] || 
-    rfid.uid.uidByte[2] != nuidPICC[2] || 
-    rfid.uid.uidByte[3] != nuidPICC[3] ) {
+  if (rfid.uid.uidByte[0] != nuidPICC[0] || rfid.uid.uidByte[1] != nuidPICC[1] || rfid.uid.uidByte[2] != nuidPICC[2] || rfid.uid.uidByte[3] != nuidPICC[3]
+        || deltatime) {
     Serial.println(F("A new card has been detected."));
 
     // Store NUID into nuidPICC array
     for (byte i = 0; i < 4; i++) {
       nuidPICC[i] = rfid.uid.uidByte[i];
     }
-   
+
     Serial.println(F("The NUID tag is:"));
     Serial.print(F("In hex: "));
     printHex(rfid.uid.uidByte, rfid.uid.size);
 
-    snprintf (msg, MSG_BUFFER_SIZE, "%d-%d-%d-%d", rfid.uid.uidByte[0], rfid.uid.uidByte[1], rfid.uid.uidByte[2], rfid.uid.uidByte[3]);
+    snprintf(msg, MSG_BUFFER_SIZE, "%d-%d-%d-%d", rfid.uid.uidByte[0], rfid.uid.uidByte[1], rfid.uid.uidByte[2], rfid.uid.uidByte[3]);
     client.publish("RestroomTime23", msg);
-
+    deltatime = 1;
     Serial.println();
     Serial.print(F("In dec: "));
     printDec(rfid.uid.uidByte, rfid.uid.size);
     Serial.println();
-  }
-  else Serial.println(F("Card read previously."));
+  } else Serial.println(F("Card read previously."));
 
-  
+
   rfid.PICC_HaltA();
 
-  
+
   rfid.PCD_StopCrypto1();
 
   if (!client.connected()) {
@@ -170,20 +164,16 @@ void loop() {
   }
   client.loop();
 
-  unsigned long now = millis();   
-  if (now - lastMsg > 2000) {
+  unsigned long now = millis();
+  if (now - lastMsg > 1000) {
+    deltatime = 0;
     lastMsg = now;
-    ++value;
-    //snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
-    //Serial.print("Publish message: ");
-    //Serial.println(msg);
-    //client.publish("RestroomTime23", msg);
   }
 }
 
 
 
-void printHex(byte *buffer, byte bufferSize) {
+void printHex(byte* buffer, byte bufferSize) {
   for (byte i = 0; i < bufferSize; i++) {
     Serial.print(buffer[i] < 0x10 ? " 0" : " ");
     Serial.print(buffer[i], HEX);
@@ -191,7 +181,7 @@ void printHex(byte *buffer, byte bufferSize) {
 }
 
 
-void printDec(byte *buffer, byte bufferSize) {
+void printDec(byte* buffer, byte bufferSize) {
   for (byte i = 0; i < bufferSize; i++) {
     Serial.print(buffer[i] < 0x10 ? " 0" : " ");
     Serial.print(buffer[i], DEC);
