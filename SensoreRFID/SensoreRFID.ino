06ccf4627ff399cdfe933fa57ff8bcd788abf5a0
maurobortolani <60657059+maurobortolani@users.noreply.github.com>
2023-06-21 10:25:35 +0200

    Update SensoreRFID.ino

--- SensoreRFID/SensoreRFID.ino
+++ SensoreRFID/SensoreRFID.ino
@@ -14,7 +14,7 @@
 #define SCREEN_WIDTH 128  // OLED display width, in pixels
 #define SCREEN_HEIGHT 64  // OLED display height, in pixels
 
-const char* ssid = "Polo_Pubblica";
+const char* ssid = "";
 const char* password = "";
 const char* mqtt_server = "broker.hivemq.com";
 
